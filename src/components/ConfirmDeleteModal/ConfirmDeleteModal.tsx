8f0d01e7cd50100a22ec8df73413647e2111eac7
Abolfazl Panahiazar <abolfazlpanahiazar@gmail.com>
2022-04-06 06:35:27 +0430

    Add delete modal UI

--- src/components/ConfirmDeleteModal/ConfirmDeleteModal.tsx
+++ src/components/ConfirmDeleteModal/ConfirmDeleteModal.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 import { Box, Button, Dialog, TextField, Typography } from "@mui/material";
 
-import {} from "./confirmDeleteModal.styles";
+import { root, container, buttonsContainer } from "./confirmDeleteModal.styles";
 
 interface IConfirmDeleteModalProps {
   open: boolean;
@@ -19,21 +19,27 @@ const ConfirmDeleteModal: FC<IConfirmDeleteModalProps> = ({
   const deleteHandler = () => {};
 
   return (
-    <Dialog open={open}>
-      <Box>
-        <Typography>آیا از تصمیم خود مطمئن هستید؟</Typography>
-      </Box>
-      <Box>
-        <Typography>
-          برای حذف مسیر ارتباطی {social_id} لطفا تایید را بنویسید
-        </Typography>
-      </Box>
-      <Box>
-        <TextField />
-      </Box>
-      <Box>
-        <Button onClick={deleteHandler}>حذف</Button>
-        <Button onClick={close}>انصراف</Button>
+    <Dialog open={open} sx={root}>
+      <Box sx={container}>
+        <Box>
+          <Typography fontSize={20}>آیا از تصمیم خود مطمئن هستید؟</Typography>
+        </Box>
+        <Box mt={2}>
+          <Typography>
+            برای حذف مسیر ارتباطی {social_id} لطفا تایید را بنویسید
+          </Typography>
+        </Box>
+        <Box mt={2}>
+          <TextField fullWidth color="warning" />
+        </Box>
+        <Box mt={2} sx={buttonsContainer}>
+          <Button onClick={close} color="warning">
+            انصراف
+          </Button>
+          <Button onClick={deleteHandler} color="error">
+            حذف
+          </Button>
+        </Box>
       </Box>
     </Dialog>
   );
--- src/components/ConfirmDeleteModal/confirmDeleteModal.styles.ts
+++ src/components/ConfirmDeleteModal/confirmDeleteModal.styles.ts
@@ -1,3 +1,15 @@
 import { SxProps, Theme } from "@mui/material";
 
-export const root: SxProps<Theme> = (theme: Theme) => ({});
+export const root: SxProps<Theme> = (theme: Theme) => ({
+  direction: "rtl",
+});
+
+export const container: SxProps<Theme> = (theme: Theme) => ({
+  padding: 2,
+});
+
+export const buttonsContainer: SxProps<Theme> = (theme: Theme) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+});
